3f555b1c4bdea130f68dcd76f9cd03ef15d0a199
Alexandwere <lazukov_ao@mail.ru>
2025-02-20 16:53:08 +0300

    chore: code style

--- src/main/java/com/javaacademy/flat_rent/FlatRentApplication.java
+++ src/main/java/com/javaacademy/flat_rent/FlatRentApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FlatRentApplication {
 
     public static void main(String[] args) {
-    SpringApplication.run(FlatRentApplication.class, args);
+        SpringApplication.run(FlatRentApplication.class, args);
 
     }
 
