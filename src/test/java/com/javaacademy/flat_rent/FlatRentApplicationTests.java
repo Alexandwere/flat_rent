8c880f3a2cd6436c3c0590237a2b2518134e3a26
Alexandwere <lazukov_ao@mail.ru>
2025-02-23 23:05:46 +0300

    feat: create services (AdvertService, ApartmentService, ClietnService)

--- src/test/java/com/javaacademy/flat_rent/FlatRentApplicationTests.java
+++ src/test/java/com/javaacademy/flat_rent/FlatRentApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FlatRentApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
